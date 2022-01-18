d4c526831f942552a523f54f556b53e87f0d558c
Nikole <kolenick7n@hotmail.com>
2022-01-18 19:48:56 -0300

    Add NoticesRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const ActionsRouter = require('./Acoes');
 const AtasRouter = require('./Atas');
 const ContactUsRouter = require('./FaleConosco');
 const InformationsRouter = require('./informacoes');
+const NoticesRouter = require('./Noticias');
 
 const routes = express.Router();
 
@@ -13,5 +14,8 @@ routes.use('/acoes', ActionsRouter);
 routes.use('/atas', AtasRouter);
 routes.use('/faleconosco', ContactUsRouter);
 routes.use('/informacoes', InformationsRouter);
+routes.use('/noticias', NoticesRouter);
+
+
 
 module.exports = routes;
