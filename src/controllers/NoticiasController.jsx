f562a4b4081408b93f706700d2fe7d82a52a6a05
Nikole <kolenick7n@hotmail.com>
2022-01-18 19:39:58 -0300

    "Noticias" CRUD

---
+++ src/controllers/NoticiasController.jsx
@@ -0,0 +1,70 @@
+const Noticias = require('../models/Noticias.jsx')
+
+module.exports = {
+  async store(req, res) {
+    try {
+      const noticias = req.body;
+      await Noticias.create(noticias);
+      res.json(noticias)
+    }
+    catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        notification: 'Internal server error while trying to create a noticias',
+      });
+    }
+  },
+  async index(req, res) {
+    try {
+      const noticias = await Noticias.find();
+      res.json(noticias)
+    }
+    catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        notification: 'Internal server error while trying to get all noticias',
+      });
+    }
+  },
+  async detail(req, res) {
+    try {
+      const { id } = req.params;
+      const noticias = await Noticias.findOne({ _id: id });
+      res.json(noticias)
+    }
+    catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        notification: 'Internal server error while trying to get a noticias by id',
+      });
+    }
+  },
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const noticias = req.body
+      const result = await Noticias.findByIdAndUpdate({ _id: id }, noticias);
+      res.json(result)
+    }
+    catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        notification:
+          'Internal server error while trying to update a noticias by id',
+      });
+    }
+  },
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const noticias = await Noticias.findByIdAndDelete({ _id: id });
+      res.json(noticias)
+    }
+    catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        notification: 'Internal server error while trying to delete a noticias',
+      });
+    }
+  }
+}
---
+++ src/routes/Noticias/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const NoticeRouter = express.Router();
+
+const NoticeController = require('../../controllers/NoticiasController');
+
+const { authenticateToken } = require('../../middlewares/authentication');
+
+NoticeRouter.get(
+  '/',
+  NoticeController.index
+);
+NoticeRouter.get(
+  '/:id',
+  NoticeController.detail
+);
+NoticeRouter.post(
+  '/',
+  NoticeController.store
+);
+NoticeRouter.put(
+  '/:id',
+  NoticeController.update
+);
+NoticeRouter.delete(
+  '/:id',
+  NoticeController.delete
+);
+
+module.exports = NoticeRouter
