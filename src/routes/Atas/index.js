05f0b55caa4fe183406363298ed85a2b4b818298
Davi_Tolen <davitolen@outlook.com>
2022-01-19 11:47:19 -0300

    "Atas" Validator

--- src/routes/Atas/index.js
+++ src/routes/Atas/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const AtasRouter = express.Router();
 
 const AtasController = require('../../controllers/AtasController.jsx');
+const AtasValidator = require('../../validators/AtasValidator.js')
 
 const { authenticateToken } = require('../../middlewares/authentication');
 
@@ -11,18 +12,22 @@ AtasRouter.get(
 );
 AtasRouter.get(
     '/:id',
+    AtasValidator.getById,
     AtasController.getById
 );
 AtasRouter.post(
     '/',
+    AtasValidator.create,
     AtasController.create
 );
 AtasRouter.put(
     '/:id',
+    AtasValidator.update,
     AtasController.update
 );
 AtasRouter.delete(
     '/:id',
+    AtasValidator.delete,
     AtasController.delete
 );
 
---
+++ src/validators/AtasValidator.js
@@ -0,0 +1,38 @@
+const { celebrate, Segments, Joi } = require('celebrate');
+
+module.exports = {
+  create: celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      type: Joi.string().valid('ATAS', 'EDITAIS').required(),
+      date: Joi.date().required(),
+      description: Joi.string().required(),
+      archive_1: Joi.string().optional(),
+      archive_2: Joi.string().optional(),
+    }),
+  }),
+
+  getById: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().required(),
+    }),
+  }),
+
+  update: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+      type: Joi.string().valid('ATAS', 'EDITAIS').required(),
+      date: Joi.date().required(),
+      description: Joi.string().required(),
+      archive_1: Joi.string().optional(),
+      archive_2: Joi.string().optional(),
+      }).min(1),
+  }),
+
+  delete: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().required(),
+      }),
+  }),
+};
