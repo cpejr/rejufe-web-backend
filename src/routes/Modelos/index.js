ab7fe3845ac966c78635d8de6a79b05ec19bda78
Davi_Tolen <davitolen@outlook.com>
2022-01-19 17:10:31 -0300

    "Modelos" Validator

--- src/routes/Modelos/index.js
+++ src/routes/Modelos/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ModelsRouter = express.Router();
 
 const ModelsController = require('../../controllers/ModelosController.jsx');
+const ModelsValidator = require('../../validators/ModelosValidator.js');
 
 const { authenticateToken } = require('../../middlewares/authentication');
 
@@ -11,18 +12,22 @@ ModelsRouter.get(
 );
 ModelsRouter.get(
     '/:id',
+    ModelsValidator.getById,
     ModelsController.getById
 );
 ModelsRouter.post(
     '/',
+    ModelsValidator.create,
     ModelsController.create
 );
 ModelsRouter.put(
     '/:id',
+    ModelsValidator.update,
     ModelsController.update
 );
 ModelsRouter.delete(
     '/:id',
+    ModelsValidator.delete,
     ModelsController.delete
 );
 
---
+++ src/validators/ModelosValidator.js
@@ -0,0 +1,38 @@
+const { celebrate, Segments, Joi } = require('celebrate');
+
+module.exports = {
+  create: celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      type: Joi.string().valid('REQUERIMENTOS ADMINISTRATIVOS', 'PETIÇÕES INICIAIS', 'JURISPRUDÊNCIA').required(),
+      date: Joi.date().required(),
+      description: Joi.string().required(),
+      archive_1: Joi.string().optional(),
+      archive_2: Joi.string().optional(),
+    }),
+  }),
+
+  getById: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().required(),
+    }),
+  }),
+
+  update: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+        type: Joi.string().valid('REQUERIMENTOS ADMINISTRATIVOS', 'PETIÇÕES INICIAIS', 'JURISPRUDÊNCIA').optional(),
+        date: Joi.date().optional(),
+        description: Joi.string().optional(),
+        archive_1: Joi.string().optional(),
+        archive_2: Joi.string().optional(),
+      }).min(1),
+  }),
+
+  delete: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().required(),
+      }),
+  }),
+};
