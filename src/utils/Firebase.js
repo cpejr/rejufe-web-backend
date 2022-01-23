c0f53fa73e01c0c23e2a752fd7989e0a8abf6e71
Davi_Tolen <davitolen@outlook.com>
2022-01-23 17:35:14 -0300

    Deleting comments

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,8 +1,5 @@
 const firebase = require('firebase/app');
 require('firebase/auth');
-// const admin = require('firebase-admin');
-
-// function initializeFirebase() {
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -12,20 +9,13 @@ const firebaseConfig = {
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
 }
-//initialize firebase  
 if (!firebase.apps.length) {
   try {
     firebase.initializeApp(firebaseConfig);
-    // admin.initializeApp({
-    //   credential: admin.credential.cert(serviceAccount),
-    //   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASEURL,
-    // });
   } catch (err) {
-    console.error(err); //eslint-disable-line
+    console.error(err); 
   }
 }
-// var db = firebase.firestore();
-// };
 
 module.exports = {
   async createNewUser(email, password) {
@@ -57,29 +47,3 @@ module.exports = {
   },
 };
 
-
-// module.exports = {
-//   // async createNewUser(email, password, user, type) {
-//   //   const result = await admin.auth().createUser({
-//   //     email,
-//   //     password,
-//   //     displayName: type,
-//   //   });
-
-//   async createUserWithEmailAndPassword(email , password){
-//     const result = await firebase
-//       .auth()
-//       .createUserWithEmailAndPassword(email, password);
-
-//     return result.user.uid;
-//   },
-
-//   async login(email, password) {
-//     const email = await this.findOne({ user });
-//     const result = await firebase
-//       .auth()
-//       .signInWithEmailAndPassword(email, password)
-
-//     return result.user;
-//   },
-// };
