45d186269f4759c0c1db326ecdf5b0b89e927825
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2022-02-03 20:02:07 -0300

    Installing and configuring ESLint

--- index.js
+++ index.js
@@ -8,28 +8,28 @@ const { errors } = require('celebrate');
 
 const routes = require('./src/routes/index');
 global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
 const app = express();
 
 mongoose.connect(process.env.DB_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-}, function (err) {
+}, (err) => {
     if (err) {
         console.log(err);
+    } else {
+        console.log('Connected to bucket');
     }
-    else {
-        console.log('Connected to bucket')
-    }
-})
+});
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 app.use(errors());
 
-app.listen(3333, function () {
-    console.log('Listening on port: 3333')
+app.listen(3333, () => {
+    console.log('Listening on port: 3333');
 });
