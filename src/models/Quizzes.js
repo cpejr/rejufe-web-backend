53ebfc2527d2bbe83493d64c4f41b5524e9a9731
JuliaTMSouza <jutarchi00@gmail.com>
2022-03-06 20:22:50 -0300

    Change to model

--- src/models/Quizzes.js
+++ src/models/Quizzes.js
@@ -1,16 +1,31 @@
 const mongoose = require('mongoose');
 
+// const toVoteSchema = new mongoose.Schema({
+//     _id: {
+//         type: String,
+//         require: true,
+//       }
+// })
+
+// const optionsSchema = new mongoose.Schema({
+//     description: {
+//         type: String,
+//         require: true,
+//     },
+//     votes: {
+//         type: Number
+//     },
+// })
+
 const QuizzesSchema = new mongoose.Schema({
     title: {
         type: String,
         require: true,
     },
-    toVote: {
-        _id: {
-          type: String,
-          require: true,
-        }
-    },
+    toVote: [{
+        type: String,
+        require: true,
+    }],
     alreadyVoted: {
         type: String,
         require: true,
@@ -23,16 +38,18 @@ const QuizzesSchema = new mongoose.Schema({
         type: Date,
         require: true,
     },
-    options: {
+    options: [{
         description: {
             type: String,
             require: true,
         },
         votes: {
-            type: Number
-        },
-    },
+            type: Number,
+        }
+    }],
 });
 
+// const Options = mongoose.model('Options', optionsSchema);
+// const ToVote = mongoose.model('ToVote', toVoteSchema);
 const Quizzes = mongoose.model('Quizzes', QuizzesSchema);
 module.exports = Quizzes;
