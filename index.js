e3fe8a8b1b584a85f45c8b85d26842ad2224870e
Nikole <kolenick7n@hotmail.com>
2022-03-11 19:24:22 -0300

    Removing console.log

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ mongoose.connect(process.env.DB_URL, {
 app.use(cors({
     origin: "http://localhost:3000",
     credentials: true
- }));
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
@@ -43,7 +43,7 @@ app.use(session({
     store: MongoStore.create({
         mongoUrl: process.env.DB_URL
     }),
-  }));
+}));
 app.use(routes)
 
 app.use(errors());
