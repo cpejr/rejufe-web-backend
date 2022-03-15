d3c0e9697cf648a2c34f5d139c238fc9836f6612
JuliaTMSouza <jutarchi00@gmail.com>
2022-03-15 14:54:43 -0300

    Fixing requisition

--- src/controllers/QuizzesController.js
+++ src/controllers/QuizzesController.js
@@ -32,8 +32,9 @@ module.exports = {
       const limit = 50;
       const times = req.query.times;
       const { id } = req.params;
-      const { date } = req.params;
-      const quizzes = await Quizzes.find({ $or:[{ toVote: id }, { alreadyVoted: id }], openingDate: {$gte: date } }).limit(limit).skip(limit * times)
+      const { date } = req.query.date;
+      console.log(req.query);
+      const quizzes = await Quizzes.find({ $or:[{ toVote: id }, { alreadyVoted: id }], openingDate: {$lte: date } }).limit(limit).skip(limit * times)
       return res.status(200).json(quizzes);
     } catch (err) {
       console.error(err);
