c5d161c1c52de833c5027e05457730ec43792e04
Davi_Tolen <davitolen@outlook.com>
2022-03-23 22:17:22 -0300

    times

--- src/controllers/InformacoesController.js
+++ src/controllers/InformacoesController.js
@@ -15,7 +15,9 @@ mongoose.connection.once("open", () => {
 module.exports = {
     async getAll(req, res) {
         try {
-            const informations = await Informations.find();
+            const limit = 50;
+            const times = req.query.times;
+            const informations = await Informations.find().limit(limit).skip(limit * times);
             return res.status(200).json(informations);
         } catch (err) {
             console.error(err);
