ab04db0a45f4c1496430a58568781a33d3756c4e
Leonardo Borges <beolorges@gmail.com>
2022-03-30 13:32:01 -0300

    mail lib implementation

---
+++ src/mail/mail.js
@@ -0,0 +1,42 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_LOGIN,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+class Email {
+  static sendEmail(request) {
+    const config = {
+      from: `${process.env.EMAIL_LOGIN}`,
+      ...request,
+    };
+    try {
+      transporter.sendMail(config);
+    } catch (error) {
+      return console.error(error);
+    }
+  }
+}
+
+module.exports = {
+  ContactUsForm(to, email, name, message) {
+    const content = `Email do associado: ${email} \n
+      Nome do associado: ${name} \n
+      Mensagem: ${message}`;
+    const subject = 'Fale Conosco - Nova Mensagem';
+
+    const emailContent = {
+      to,
+      subject,
+      text: content,
+    };
+    return Email.sendEmail(emailContent);
+  },
+};
