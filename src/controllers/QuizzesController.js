8b7cd77e51b2805fc3e58a3e91bdf9917a29d21a
JuliaTMSouza <jutarchi00@gmail.com>
2022-07-06 11:11:18 -0300

    Not able to vote if quizz is closed

--- src/controllers/QuizzesController.js
+++ src/controllers/QuizzesController.js
@@ -77,34 +77,29 @@ module.exports = {
     }
   },
   async update(req, res) {
-    try {
-      const { id } = req.params;
-      const quizzes = req.body;
-      const keys = Object.keys(quizzes);
-      const date = new date();
-      const quizz = await Quizzes.findOne({ _id: id });
-      if (keys.find((key) => key === options)) {
-        if (quizz.closingDate > date) {
-          const result = await Quizzes.findByIdAndUpdate({ _id: id }, quizzes);
-          return res.status(200).json({_id:result?.id, alreadyVoted:result?.alreadyVoted, closingDate: result?.closingDate, description: result?.description, openingDate: result?.openingDate, title: result?.title, toVote: result?.toVote});
-        } else {
-          return res.status(500).json({
-            notification:
-              'Internal server error while trying to update a quiz by id',
-          }); 
-        }
-      } else {
+    const { id } = req.params;
+    const date = new Date();
+    const quizz = await Quizzes.findOne({ _id: id });
+    if (quizz.closingDate > date) {
+      try {
+        const quizzes = req.body;
         const result = await Quizzes.findByIdAndUpdate({ _id: id }, quizzes);
         return res.status(200).json({_id:result?.id, alreadyVoted:result?.alreadyVoted, closingDate: result?.closingDate, description: result?.description, openingDate: result?.openingDate, title: result?.title, toVote: result?.toVote});
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+          notification:
+            'Internal server error while trying to update a quiz by id',
+        });
       }
-    } catch (err) {
-      console.error(err);
+    } else {
       return res.status(500).json({
         notification:
-          'Internal server error while trying to update a quiz by id',
-      });
+          'Não é mais possível votar/alterar o quizz',
+      }); 
     }
   },
+
   async delete(req, res) {
     try {
       const { id } = req.params;
