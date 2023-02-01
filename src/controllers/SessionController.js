8723769f44e51843e98736831a5a57b4bf60e6c6
João Pedro Lima Pirajá <jplp100@hotmail.com>
2023-02-01 17:23:39 -0300

    fix: more deploy fixes 4

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -41,7 +41,14 @@ module.exports = {
         : 1000 * 60 * 60 * 8; // 5 dias se true ou 8 horas se false
       request.session.user = user;
 
-      return response.status(200).json({ user, accessToken });
+      req.session.regenerate((err) => {
+        if (err) {
+          return response.status(500).json({
+            notification: 'Internal server error while trying to get User',
+          });
+        }
+        return response.status(200).json({ user, accessToken });
+      });
     } catch (error) {
       console.warn(error);
       return response.status(500).json({
